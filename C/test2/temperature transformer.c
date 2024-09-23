3ae092f4cba2716aafa4589ce3b75a78de5c2a11
Tony Jia <tonyjia1028@gmail.com>
2024-09-23 11:30:28 +0800

    1

--- C/test2/temperature transformer.c
+++ C/test2/temperature transformer.c
@@ -10,7 +10,7 @@ int main()
         printf("请输入你要转换的摄氏温度:\n");
         scanf("%d",&c1);
         double f = c1 * 1.8 + 32;
-        printf("转换后的温度为:%f\n",f);
+        printf("转换后的温度为:%.2f\n",f);
     }
     else if (t == 'F')
     {
@@ -18,7 +18,7 @@ int main()
         printf("请输入你要转换的华氏温度:\n");
         scanf("%d",&f1);
         double C = (f1 - 32) * 5.0 / 9.0;
-        printf("转换后的温度为:%f\n",C);
+        printf("转换后的温度为:%.2f\n",C);
     }
     else
     {
