e83d6f1cf1df705e029317fd720812a9f7f30fa6
Tony Jia <tonyjia1028@gmail.com>
2024-09-23 21:00:32 +0800

    exchange

---
+++ C/test3/exchange.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    double p,fu;
+    printf("Please insert the total price:\n");
+    scanf("%lf",&p);
+    printf("Please insert the payment:\n");
+    scanf("%lf",&fu);
+    if (fu < p){
+        printf("Payment not enough!\n");
+    }
+    if (fu >= p)
+    {
+        double ex = fu - p;
+        printf("The exchange is:%.2lf\n",ex);
+    }
+    return 0;
+}
