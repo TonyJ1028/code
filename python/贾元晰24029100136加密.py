cee8e78f81f863fa3a2c5cec383ea74e5e8113b8
Tony Jia <tonyjia1028@gmail.com>
2024-10-14 11:06:25 +0800

    while

--- python/贾元晰24029100136加密.py
+++
@@ -1,7 +0,0 @@
-pc = input("please insert the plaincode:")
-for p in pc:
-    if ord("a") <= ord(p) <=ord("z"):
-        print(chr(ord("a") + (ord(p) - ord("a")+3)%26), end='')
-    else:
-        print(p, end='')
-        
